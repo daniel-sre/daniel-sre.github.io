be43ebae6cecfe7adffaa0b52180408004b0dc7f
Roots <roots@192.168.0.104>
2026-04-26 10:56:35 +0800

    update index

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,36 +1,61 @@
 export default {
-  title: "Daniel Docs",
-  description: "SRE / DevOps Notes",
-
   themeConfig: {
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Go", link: "/go/" },
-      { text: "Network", link: "/network/" },
-      { text: "System", link: "/system/" },
-      { text: "Tools", link: "/tools/" }
-    ],
-
     sidebar: [
       {
-        text: "Go",
-        items: [
-          { text: "Overview", link: "/go/" },
-
-	  {
-	    text: "Basic",
-	    items: [
-		{ text: "Overview", link: "/go/basic/" },
-        	{ text: "Slice", link: "/go/basic/slice" },
-        	{ text: "Map", link: "/go/basic/map" },
-        	{ text: "Struct", link: "/go/basic/struct" },
-        	{ text: "Interface", link: "/go/basic/interface" }
-           ]
-      	  },
-	   { text: "Concurrency", link: "/go/concurrency" },
-	   { text: "Memory", link: "/go/memory" }
-	]
+        text: "Overview",
+        link: "/"
       },
+
+      {
+        text: "Basic",
+        items: [
+          { text: "Overview", link: "/go/basic/" },
+
+          {
+            text: "Slice",
+            items: [
+              { text: "Overview", link: "/go/basic/slice/" },
+              { text: "底层结构与扩容机制", link: "/go/basic/slice/core" }
+            ]
+          },
+
+          {
+            text: "Map",
+            items: [
+              { text: "Overview", link: "/go/basic/map/" }
+            ]
+          },
+
+          {
+            text: "Struct",
+            items: [
+              { text: "Overview", link: "/go/basic/struct/" }
+            ]
+          },
+
+          {
+            text: "Interface",
+            items: [
+              { text: "Overview", link: "/go/basic/interface/" }
+            ]
+          }
+        ]
+      },
+
+      {
+        text: "Concurrency",
+        items: [
+          { text: "Overview", link: "/go/concurrency/" }
+        ]
+      },
+
+      {
+        text: "Memory",
+        items: [
+          { text: "Overview", link: "/go/memory/" }
+        ]
+      },
+
       {
         text: "Network",
         items: [
@@ -39,14 +64,16 @@ export default {
           { text: "HTTP", link: "/network/http" }
         ]
       },
+
       {
         text: "System",
         items: [
           { text: "Overview", link: "/system/" },
           { text: "Linux", link: "/system/linux" },
-          { text: "IO Model", link: "/system/io" }
+          { text: "IO Model", link: "/system/io-model" }
         ]
       },
+
       {
         text: "Tools",
         items: [
@@ -54,10 +81,6 @@ export default {
           { text: "Go Port Scanner", link: "/tools/go-port-scanner" }
         ]
       }
-    ],
-
-    socialLinks: [
-      { icon: "github", link: "https://github.com/daniel-sre" }
     ]
   }
 }
